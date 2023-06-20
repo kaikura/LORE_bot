2b8e08d87baddceef99e67fe34d11a495083f401
kaikura <antonionic94.gmail.com>
2023-06-20 17:36:07 +0200

    up

--- commands/lore.js
+++ commands/lore.js
@@ -33,10 +33,11 @@ module.exports = {
 
 		const embed = new EmbedBuilder()
 			.setColor(0x0099FF)
-			.setTitle('LORE')
+			.setImage('https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75')
+			//.setTitle('LORE')
 			//.setURL('https://discord.js.org/')
-			.setAuthor({ name: 'Citizen Lore', iconURL: 'https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75' })
-			.setDescription('Lore description')
+			//.setAuthor({ name: 'Citizen Lore', iconURL: 'https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75' })
+			//.setDescription('Lore description')
 			//.setThumbnail('https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75')
 			.addFields(
 				{ name: 'Chapter I', value: 'This is the biginning of the story' },
@@ -45,16 +46,16 @@ module.exports = {
 				//{ name: 'Inline field title', value: 'Some value here', inline: true },
 			)
 			//.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-			.setImage('https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75')
-			.setTimestamp()
-			.setFooter({ text: 'LabLore', iconURL: 'https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75' });
+			//.setImage('https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75')
+			//.setTimestamp()
+			.setFooter({ text: 'Powered by PSLab', iconURL: 'https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75' });
 		
 
 		const row = new ActionRowBuilder()
 			.addComponents(cancel, confirm);
 
 		const response = await interaction.reply({
-			content: `Ready to start a new story?`,
+			//content: ``,
 			embeds : [embed],
 			components: [row],
 			ephemeral: true,
