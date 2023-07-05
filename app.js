e462638dd87a48bbf84266c294ebe17ba798d23b
kaikura <antonionic94.gmail.com>
2023-07-06 01:34:21 +0200

    new

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, GatewayIntentBits, Events } = require('discord.js');
-const { EmbedBuilder ,ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require('discord.js');
+const { EmbedBuilder ,ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuOptionBuilder ,StringSelectMenuBuilder, AttachmentBuilder} = require('discord.js');
 const sqlite3 = require('sqlite3').verbose();
 const token = process.env.DISCORD_TOKEN;
 
@@ -13,12 +13,11 @@ const client = new Client({
 
 	 ] });
 
-const currentUsers = []; //keeping track of current users
 const currentLevel = new Map();//keeping track of the current level
 
 async function botBusy(interaction,string){
 
-	if(arguments.length==1) string ='Already playing a game....';
+	if(arguments.length==1) string =`${interaction.user} you are already playing a game....`;
 
 	await interaction.reply({
 		content : string,
@@ -27,19 +26,26 @@ async function botBusy(interaction,string){
 	
 }
 
+function getdate() {
+	const currentDate = new Date();
+	const logDate = currentDate.toLocaleString();
+	return logDate;
+  }
+
 //GAME STARTS	
 async function startinGame(interaction){
 
-	if (currentUsers.indexOf(interaction.user.id) > -1){
+	//const role = interaction.member.roles.cache.find(r => r.name === "Polygon Citizen");
+
+	if (currentLevel.get(`${interaction.user.id}`)){
 		botBusy(interaction);
 		return;
 	}
-
-	currentUsers.push(interaction.user.id);
-	console.log(`${interaction.user.username} has started a game`);
-	console.log(currentUsers);
-	currentLevel.set(`${interaction.user.id}`,{level : 0});
+	//DEBUGSHIT
+	console.log(`${getdate()} - ${interaction.user.username} has started a game`);
+	currentLevel.set(`${interaction.user.id}`,{level : 0, story: interaction.customId});
 	console.log(currentLevel);
+	////////////
 
 	const db = new sqlite3.Database('./lore.db', sqlite3.OPEN_READWRITE, (err) => {
 		if (err) {
@@ -49,7 +55,7 @@ async function startinGame(interaction){
 	});
 
 	const firstMex = await new Promise((resolve, reject) => {
-		db.get(`SELECT story,L1,L2,L3,L1txt,L2txt,L3txt FROM lore where id ='000001' and storyId=1`, (err, result) => {
+		db.get(`SELECT story,L1,L2,L3,L1txt,L2txt,L3txt FROM lore where id ='000001' and storyId=${interaction.customId}`, (err, result) => {
 			if (err) {
 				console.log('Error running sql: ' + sql)
 				console.log(err)
@@ -57,32 +63,35 @@ async function startinGame(interaction){
 			} else {
 			/////////////////
 			//INITIAL MESSAGE
-			const confirm = new ButtonBuilder()
+			
+			const file = new AttachmentBuilder('imgs/lab1.png');
+			const sel1 = new ButtonBuilder()
 				.setCustomId(`${result.L1}`)
 				.setLabel(`${result.L1txt}`)
 				.setStyle(ButtonStyle.Primary);
 
-			const cazz = new ButtonBuilder()
-				.setCustomId(`${result.L2}`)
-				.setLabel(`${result.L2txt}`)
-				.setStyle(ButtonStyle.Primary);
-
-			const cancel = new ButtonBuilder()
+			const sel2 = new ButtonBuilder()
 				.setCustomId(`${result.L3}`)
 				.setLabel(`${result.L3txt}`)
 				.setStyle(ButtonStyle.Primary);
-				
+			/*
+			const sel3 = new ButtonBuilder()
+				.setCustomId(`${result.L3}`)
+				.setLabel(`${result.L3txt}`)
+				.setStyle(ButtonStyle.Primary);
+			*/	
 			const row = new ActionRowBuilder()
-				.addComponents(confirm,cazz,cancel);
+				.addComponents(sel1,sel2);
 
 
 			const embed = new EmbedBuilder()
 				.setColor(0x0099FF)
-				.setImage('https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75')
+				//.setImage('attachment://lab1.png')
 				.setDescription(`${result.story}`)
-				.setFooter({ text: 'Powered by PSLab', iconURL: 'https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75' });
-			
+
+
 			const msg = {
+				files : [file],
 				embeds : [embed],
 				components: [row],
 				ephemeral: true,
@@ -93,28 +102,26 @@ async function startinGame(interaction){
 		});
 	});
 	db.close();
-	await interaction.reply(`${interaction.user} has started a game`);
-	await interaction.followUp(firstMex);
+	await interaction.reply(firstMex);
 
 }//strGame
 
-/////////////////////////////////
+
 //GAME
 async function game(interaction){
 
-	if(!currentLevel.get(`${interaction.user.id}`)){
-		await interaction.reply({
-			content : 'You already made your choices please start a new game!',
-			ephemeral : true
-		});
-		return;
-	} 
-
-	if(Number(interaction.customId)<=currentLevel.get(`${interaction.user.id}`).level){
-
-		botBusy(interaction,'You already took that step! Please go on...');
+	//CONSISTENCY CHECK
+	if (!(currentLevel.get(`${interaction.user.id}`))){
+		botBusy(interaction,`${interaction.user} Please start a new game`);
 		return;
 	}
+
+	if(Number(interaction.customId)<Number(currentLevel.get(`${interaction.user.id}`).level)){
+
+		botBusy(interaction,'You already took that step! Please move on.');
+		return;
+	}
+	//CONSISTENCY CHECK
 
 
 	const db = new sqlite3.Database('./lore.db', sqlite3.OPEN_READWRITE, (err) => {
@@ -126,7 +133,7 @@ async function game(interaction){
 
 
 	let follow = await new Promise((resolve, reject) => {
-		db.get(`SELECT story,L1,L2,L3,L1txt,L2txt,L3txt FROM lore where id =${interaction.customId} and storyId=1`, (err, result) => {
+		db.get(`SELECT story,L1,L2,L3,L1txt,L2txt,L3txt FROM lore where id =${interaction.customId} and storyId=${currentLevel.get(`${interaction.user.id}`).story}`, (err, result) => {
 			if (err) {
 			console.log('Error running sql: ');
 			console.log(err);
@@ -134,47 +141,57 @@ async function game(interaction){
 			} else {
 				if(!result){	
 
+				currentLevel.delete(`${interaction.user.id}`);
 				resolve(0);
 
 				}else{
-				currentLevel.set(`${interaction.user.id}`,{level : Math.min(Number(result.L1),Number(result.L2),Number(result.L3))});
+				currentLevel.set(`${interaction.user.id}`,{level : Math.min(Number(result.L1),Number(result.L2),Number(result.L3)), story:currentLevel.get(`${interaction.user.id}`).story});
 				console.log(currentLevel);
+				const file = new AttachmentBuilder('imgs/lab2.png');
 				const nn = new ButtonBuilder()
-					.setCustomId(`'${result.L1}'`)
+					.setCustomId(`${result.L1}`)
 					.setLabel(`${result.L1txt}`)
 					.setStyle(ButtonStyle.Primary);
 				
 				const nn2 = new ButtonBuilder()
-					.setCustomId(`'${result.L2}'`)
-					.setLabel(`${result.L2txt}`)
-					.setStyle(ButtonStyle.Primary);
-			
-				const nn3 = new ButtonBuilder()
-					.setCustomId(`'${result.L3}'`)
+					.setCustomId(`${result.L3}`)
 					.setLabel(`${result.L3txt}`)
 					.setStyle(ButtonStyle.Primary);
-			
+				/*
+				const nn3 = new ButtonBuilder()
+					.setCustomId(`${result.L3}`)
+					.setLabel(`${result.L3txt}`)
+					.setStyle(ButtonStyle.Primary);
+				*/
 				const nrow = new ActionRowBuilder()
-					.addComponents(nn,nn2,nn3);
+					.addComponents(nn,nn2);
 
 				const embed = new EmbedBuilder()
 					.setColor(0x0099FF)
-					.setImage('https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75')
+					//.setImage('attachment://lab1.png')
 					.setDescription(`${result.story}`)
-					.setFooter({ text: 'Powered by PSLab', iconURL: 'https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75' });
 
 				if(!(result.L1 && result.L2 && result.L3)){
-					const index = currentUsers.indexOf(interaction.user.id);
-					const x = currentUsers.splice(index, 1);
+					const file = new AttachmentBuilder('imgs/lab3.png');
 					currentLevel.delete(`${interaction.user.id}`);
+					console.log(currentLevel);
+					const endembed = new EmbedBuilder()
+					.setColor(0x0099FF)
+					.setDescription(`${result.story}`)
+					.addFields(
+						{ name: '\u200B', value: '\u200B' },
+					)
+					.setFooter({ text: 'Powered by PSLab', iconURL: 'https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75' });
 					const msg = {
-						embeds : [embed],
+						files : [file],
+						embeds : [endembed],
 						components: [],
 						ephemeral: true,
 						}
 					resolve(msg);
 				}
 				const msg = {
+				files : [file],
 				embeds : [embed],
 				components: [nrow],
 				ephemeral: true,
@@ -191,9 +208,8 @@ async function game(interaction){
 		await interaction.reply(follow);
 	}
 	else{
-		const index = currentUsers.indexOf(interaction.user.id);
-		const x = currentUsers.splice(index, 1);
-		currentLevel.set(`${interaction.user.id}`,{level : 0});
+		currentLevel.delete(`${interaction.user.id}`);
+		console.log(currentLevel);
 		await interaction.reply({
 			content : 'The story ends here at the moment...',
 			ephemeral: true,
@@ -205,6 +221,64 @@ async function game(interaction){
 }//END GAME
 /////////////////
 
+async function roleSelection(interaction){
+
+	if (currentLevel.get(`${interaction.user.id}`)){
+		botBusy(interaction);
+		return;
+	}
+
+	const embed = new EmbedBuilder()
+	.setColor(0x0099FF)
+	.setTitle('Choose the Path')
+	//.setURL('https://discord.js.org/')
+
+	.setDescription('XXXXXXXXXXXX')
+	.setFooter({ text: 'Powered by PSLab', iconURL: 'https://lab.ps-lab.io/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FlabLogoBlack.673a99a0.gif&w=3840&q=75' });
+
+	const b1=new ButtonBuilder()
+			.setCustomId('0')
+			.setLabel('SG Citizen')
+			.setStyle(ButtonStyle.Danger)
+	const b2=new ButtonBuilder()
+			.setCustomId('1')
+			.setLabel('Polygon Citizen')
+			.setStyle(ButtonStyle.Danger)
+	const b3=new ButtonBuilder()
+			.setCustomId('2')
+			.setLabel('Citizen of the Dead')
+			.setStyle(ButtonStyle.Danger)
+
+	const btn = {b1,b2,b3};
+
+
+
+	if(!interaction.member.roles.cache.find(role => role.name === "Polygon Citizen")){b2.setDisabled(true)};
+	if(!interaction.member.roles.cache.find(role => role.name === "Citizen of the Dead")){b3.setDisabled(true)};
+	if(!interaction.member.roles.cache.find(role => role.name === "SG Citizen")){b1.setDisabled(true)};
+
+	const row = new ActionRowBuilder()
+		.addComponents(b1,b2,b3);
+
+	await interaction.reply(`${interaction.user} has started a game.`);
+	await interaction.followUp({
+		//content: 'Select your path!',
+		//embeds : [embed],
+		components: [row],
+		ephemeral : true,
+	});
+
+}
+
+////////////////
+
+async function visitor(interaction){
+
+	await interaction.reply({
+		content : `Dear ${interaction.user.username}, there is no story available for you at the moment`,
+		ephemeral : true,
+	});
+}
 
 //EVENTS LISTENER
 /////////////////
@@ -214,21 +288,32 @@ client.once('ready', (c) =>{
 	console.log(`✅ ${c.user.tag} is online`);
 });
 
-
-
 client.on('interactionCreate', (interaction) => {
 
 	if (interaction.isChatInputCommand()) {
 		if (interaction.commandName == 'lore'){
+			if(interaction.member.roles.cache.find(role => role.name === "Polygon Citizen" || role.name === "SG Citizen" || role.name === "Citizen of the Dead")){
+				
+				roleSelection(interaction);		
+				
+			}else{
 
-			startinGame(interaction);
+				visitor(interaction);
+			}		
 		}
 	}
 	else if (interaction.isButton()) {
+		if(interaction.customId <= 2){
 
-		game(interaction);
+			startinGame(interaction);
+
+		}else{
+			game(interaction);
+		}
+
+		
 	
-}
+	} 
 });
 
 ///////////////////
